8c8185381ae7428501f4728b8e20b0a4e3a31d5f
Patrick Piemonte <patrick@diy.org>
2014-04-29 18:19:59 -0700

    update flow layouts

--- Source/PBJHexagonFlowLayout.m
+++ Source/PBJHexagonFlowLayout.m
@@ -2,16 +2,47 @@
 //  PBJHexagonFlowLayout.m
 //
 //  Created by Patrick Piemonte on 10/30/13.
-//  Copyright (c) 2013. All rights reserved.
+//  Copyright (c) 2013-present, Patrick Piemonte, http://patrickpiemonte.com
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "PBJHexagonFlowLayout.h"
 
+CG_INLINE CGFloat CGFloat_nearbyint(CGFloat cgfloat) {
+#if defined(__LP64__) && __LP64__
+    return nearbyint(cgfloat);
+#else
+    return nearbyintf(cgfloat);
+#endif
+}
+
+CG_INLINE CGFloat CGFloat_floor(CGFloat cgfloat) {
+#if defined(__LP64__) && __LP64__
+    return floor(cgfloat);
+#else
+    return floorf(cgfloat);
+#endif
+}
+
 @interface PBJHexagonFlowLayout ()
 {
     NSInteger _itemsPerRow;
-    NSInteger _itemTotalCount;
-    CGSize _hexagonSize;
 }
 
 @end
@@ -19,8 +50,6 @@
 @implementation PBJHexagonFlowLayout
 
 @synthesize itemsPerRow = _itemsPerRow;
-@synthesize itemTotalCount = _itemTotalCount;
-@synthesize hexagonSize = _hexagonSize;
 
 #pragma mark - UICollectionViewLayout Subclass hooks
 
@@ -30,32 +59,32 @@
     
     if (_itemsPerRow == 0)
         _itemsPerRow = 4;
-    _itemTotalCount = [self.collectionView numberOfItemsInSection:0];
 }
 
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
 {
-    NSMutableArray *attributes = [[NSMutableArray alloc] init];
+    NSMutableArray *layoutAttributes = [[NSMutableArray alloc] init];
+    NSInteger numberOfItems = [self.collectionView numberOfItemsInSection:0];
     
-    for (NSInteger i = 0 ; i < _itemTotalCount; i++) {
+    for (NSInteger i = 0 ; i < numberOfItems; i++) {
         NSIndexPath *indexPath = [NSIndexPath indexPathForItem:i inSection:0];
-        [attributes addObject:[self layoutAttributesForItemAtIndexPath:indexPath]];
+        [layoutAttributes addObject:[self layoutAttributesForItemAtIndexPath:indexPath]];
     }
-    return attributes;
+    return layoutAttributes;
 }
 
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSInteger row = (NSInteger)( floorf((indexPath.row / _itemsPerRow)) );
+    NSInteger row = (NSInteger) CGFloat_nearbyint( CGFloat_floor(indexPath.row / _itemsPerRow) );
     NSInteger col = indexPath.row % _itemsPerRow;
 
-    CGFloat horiOffset = ((row % 2) != 0) ? 0 : _hexagonSize.width * 0.5f;
+    CGFloat horiOffset = ((row % 2) != 0) ? 0 : self.itemSize.width * 0.5f;
     CGFloat vertOffset = 0;
     
     UICollectionViewLayoutAttributes *attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
-    attributes.size = _hexagonSize;
-    attributes.center = CGPointMake( ( (col * _hexagonSize.width) + (0.5f * _hexagonSize.width) + horiOffset),
-                                     ( ( (row * 0.75f) * _hexagonSize.height) + (0.5f * _hexagonSize.height) + vertOffset) );
+    attributes.size = self.itemSize;
+    attributes.center = CGPointMake( ( (col * self.itemSize.width) + (0.5f * self.itemSize.width) + horiOffset),
+                                     ( ( (row * 0.75f) * self.itemSize.height) + (0.5f * self.itemSize.height) + vertOffset) );
     return attributes;
 }
 
@@ -66,8 +95,11 @@
 
 - (CGSize)collectionViewContentSize
 {
+    NSInteger numberOfItems = [self.collectionView numberOfItemsInSection:0];
+
     CGFloat contentWidth = self.collectionView.bounds.size.width;
-    CGFloat contentHeight = ( ((_itemTotalCount / _itemsPerRow) * 0.75f) * _hexagonSize.height) + (0.5f + _hexagonSize.height);
+    CGFloat contentHeight = ( ((numberOfItems / _itemsPerRow) * 0.75f) * self.itemSize.height) + (0.5f + self.itemSize.height);
+    
     CGSize contentSize = CGSizeMake(contentWidth, contentHeight);
     return contentSize;
 }
